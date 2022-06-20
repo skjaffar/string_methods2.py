1be70e1eb78727ffdb29d1e51eee86aa34a05aed
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-20 11:16:23 +0530

    Add files via upload

---
+++ strings_methods.py
@@ -0,0 +1,52 @@
+# print('hey PYthon'.capitalize())
+
+# print('hey PYthon'.title())
+
+# print('python'.upper())
+
+# print('PYTHON'.lower())
+
+# print('hai my dear friend'.islower())
+
+# print('HAI MY DEAR FRIEND'.isupper())
+
+# b='hai jaffar'
+# print(b.replace('hai','hey'))
+# print(b.replace('jaffar',str(3)))
+
+# f="world"
+# print('world'.count('r'))
+
+# print('hello'.count('o'))
+
+# m='myname'
+# print(m.count('n'))
+
+# print('hello'.count('e'))
+
+# print(len('myname'))
+
+# h='jaffar'
+# print(h)
+#  print(h[0])
+#  print(h[1])
+# print(h[2])
+# print(h[3])
+# print(h[4])
+# print(h[5])
+
+# print(h[-1])
+# print(h[-2])
+# print(h[-3])
+# print(h[-4])
+# print(h[-5])
+
+# k='jaffar'
+# print(k[0]+k[-2])
+# print('jaffar'.find('j'))
+# print('jaffar'.find('a'))
+# print('jaffar'.find('f'))
+# print('jaffar'.find('f'))
+# print('jaffar'.find('a'))
+# print('jaffar'.find('r'))
+
